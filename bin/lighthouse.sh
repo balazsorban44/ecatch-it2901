11d4ce0f2fac1da11e6262dd65711e430001d02c
Balázs Orbán <info@balazsorban.com>
2019-01-24 11:03:54 +0100

    📝 provide detailed report when Lighthouse runs

--- bin/lighthouse.sh
+++ bin/lighthouse.sh
@@ -5,16 +5,16 @@
 sleep 15
 
 # NOTE: Add tresholds
-TRESHOLDS="--pwa=1"
+FLAGS="--runner=wpt --pwa=1"
 
 if [ "$TRAVIS_PULL_REQUEST" != false ]
   then
   if [ "$TRAVIS_BRANCH" = "master" ]
   then
-    yarn lh $TRESHOLDS https://ecatch-kyst.firebaseapp.com
+    yarn lh "$FLAGS" https://ecatch-kyst.firebaseapp.com
   elif [ "$TRAVIS_BRANCH" = "develop" ]
   then
-    yarn lh $TRESHOLDS https://ecatch-kyst-beta.firebaseapp.com
+    yarn lh "$FLAGS" https://ecatch-kyst-beta.firebaseapp.com
   fi
 fi
 
